7844f08f62ca229c0d73ae43b6d166956311169a
outclass <mhlee@wemb.co.kr>
2022-08-19 13:09:08 +0900

    Feat: Set Observer Patter

---
+++ src/pages/ObservablePractice.tsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header";
+import ObservableMain from "../components/observable/ObservableMain";
+import Layout from "./Layout";
+
+export default function ObservablePractice () {
+  return(
+    <Layout>
+      <ObservableMain/>
+    </Layout>
+  )
+}
